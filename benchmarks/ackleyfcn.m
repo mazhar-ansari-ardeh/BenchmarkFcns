e76f5c6cfda5b2ddff072eb35f4a7a72edc40702
mzhr <mzhr@mazhr>
2016-08-11 22:27:41 +0430

    Added help comments

--- benchmarks/ackleyfcn.m
+++ benchmarks/ackleyfcn.m
@@ -1,3 +1,14 @@
+% Computes the value of Ackley benchmark function.
+% SCORES = ACKLEYFCN(X) computes the value of the Ackey function at point
+% X. ACKLEYFCN accepts a matrix of size M-by-N and returns a vetor SCORES
+% of size M-by-1 in which each row contains the function value for each row
+% of X.
+% For more information please visit: 
+% https://en.wikipedia.org/wiki/Test_functions_for_optimization
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
 function scores = ackleyfcn(x)
     n = size(x, 2);
     ninverse = 1 / n;
