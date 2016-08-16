92d21234ce4643d60c5057898f9a691641ae2786
mzhr <mzhr@mazhr>
2016-08-16 21:21:25 +0430

    Added the Matyas function

---
+++ benchmarks/matyasfcn.m
@@ -0,0 +1,19 @@
+% Computes the value of the Matyas benchmark function.
+% SCORES = MATYASFCN(X) computes the value of the Matyas function at 
+% point X. MATYASFCN accepts a matrix of size M-by-2 and returns a  
+% vetor SCORES of size M-by-1 in which each row contains the function value 
+% for the corresponding row of X.
+% For more information please visit: 
+% https://en.wikipedia.org/wiki/Test_functions_for_optimization
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
+function scores = matyasfcn(x)
+    n = size(x, 2);
+    assert(n == 2, 'Matyas''s functions is only defined on a 2D space.')
+    X = x(:, 1);
+    Y = x(:, 2);
+    
+    scores = 0.26 * (X .^ 2 + Y.^2) - 0.48 * X .* Y;
+end
