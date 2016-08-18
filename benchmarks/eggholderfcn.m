724d875d24920aa3f78ca94c34c7e9745f4f015f
mzhr <mzhr@mazhr>
2016-08-18 20:01:15 +0430

    Added help comments for the Eggholder function

--- benchmarks/eggholderfcn.m
+++ benchmarks/eggholderfcn.m
@@ -1,3 +1,14 @@
+% Computes the value of the Eggholder benchmark function.
+% SCORES = EGGHOLDERFCN(X) computes the value of the Eggholder
+% function at point X. EGGHOLDERFCN accepts a matrix of size M-by-2 
+% and returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for the corresponding row of X. For more information 
+% please visit: 
+% https://en.wikipedia.org/wiki/Test_functions_for_optimization
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
 function scores = eggholderfcn(x)
     n = size(x, 2);
     assert(n == 2, 'The Eggholder function is only defined on a 2D space.')
