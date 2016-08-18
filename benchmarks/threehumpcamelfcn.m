abe8ab0caa891ba45cb0c609899016b18a485fde
mzhr <mzhr@mazhr>
2016-08-18 20:00:24 +0430

    Added the Three-hump camel function

---
+++ benchmarks/threehumpcamelfcn.m
@@ -0,0 +1,20 @@
+% Computes the value of the Three-hump camel benchmark function.
+% SCORES = THREEHUMPCAMELFCN(X) computes the value of the Three-hump camel   
+% function at point X. THREEHUMPCAMELFCN accepts a matrix of size M-by-2  
+% and returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for the corresponding row of X.
+% For more information please visit: 
+% https://en.wikipedia.org/wiki/Test_functions_for_optimization
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
+function scores = threehumpcamelfcn(x)
+    
+    n = size(x, 2);
+    assert(n == 2, 'The Three-hump camel function is only defined on a 2D space.')
+    X = x(:, 1);
+    Y = x(:, 2);
+    
+    scores = (2 * X .^ 2) - (1.05 * (X .^ 4)) + ((X .^ 6) / 6) + X .* Y + Y .^2;
+end
