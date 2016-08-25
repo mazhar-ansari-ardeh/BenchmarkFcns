263fe0dfb98975a9b7c16db3589399d8aab80d9b
mzhr <mzhr@mazhr>
2016-08-25 13:17:09 +0430

    Added help comments

--- benchmarks/zakharovfcn.m
+++ benchmarks/zakharovfcn.m
@@ -1,3 +1,12 @@
+% Computes the value of Zakharov benchmark function.
+% SCORES = ZAKHAROVFCN(X) computes the value of the Zakharov function at 
+% point X. ZAKHAROVFCN accepts a matrix of size M-by-N and returns a vetor 
+% SCORES of size M-by-1 in which each row contains the function value for
+% each row of X.
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
 function scores = zakharovfcn(x)
 
     n = size(x, 2);
