092ad425db1dd6b8c869fb83dc75535e70f89117
mzhr <mzhr@mazhr>
2016-08-25 11:26:15 +0430

    Added comments to the Griewank function.

--- benchmarks/griewankfcn.m
+++ benchmarks/griewankfcn.m
@@ -1,3 +1,12 @@
+% Computes the value of the Griewank benchmark function.
+% SCORES = GRIEWANKFCN(X) computes the value of the Griewank's
+% function at point X. GRIEWANKFCN accepts a matrix of size M-by-N 
+% and returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for the corresponding row of X.
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
 function scores = griewankfcn(x)
     
     n = size(x, 2);
