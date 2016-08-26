c0765cbfae7f82312ab388182202c81ab9a753f2
mzhr <mzhr@mazhr>
2016-08-26 23:35:12 +0430

    Corrected the name of the function in the context.

--- benchmarks/bukinn6fcn.m
+++ benchmarks/bukinn6fcn.m
@@ -1,6 +1,6 @@
-% Computes the value of the Bulkin N. 6 benchmark function.
-% SCORES = BULKINN6FCN(X) computes the value of the Bulkin N. 6 function at 
-% point X. BULKINN6FCN accepts a matrix of size M-by-2 and returns a  
+% Computes the value of the Bukin N. 6 benchmark function.
+% SCORES = BUKINN6FCN(X) computes the value of the Bukin N. 6 function at 
+% point X. BUKINN6FCN accepts a matrix of size M-by-2 and returns a  
 % vetor SCORES of size M-by-1 in which each row contains the function value 
 % for the corresponding row of X.
 % For more information please visit: 
@@ -11,7 +11,7 @@
 % Google's e-mail service or feel free to kindly modify the repository.
 function scores = bukinn6fcn(x)
     n = size(x, 2);
-    assert(n == 2, 'The Bulkin N. 6 functions is only defined on a 2D space.')
+    assert(n == 2, 'The Bukin N. 6 functions is only defined on a 2D space.')
     
     X = x(:, 1);
     X2 = X .^ 2;
