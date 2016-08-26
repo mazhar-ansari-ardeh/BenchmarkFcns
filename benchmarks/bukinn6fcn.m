e8e73f5065728ecf4ef493fc3837f45849f5b994
mzhr <mzhr@mazhr>
2016-08-26 22:59:36 +0430

    Added Latex version of the Bukin N.6 and Booth functions.

--- benchmarks/bukinn6fcn.m
+++ benchmarks/bukinn6fcn.m
@@ -1,6 +1,6 @@
-% Computes the value of the Bulkin benchmark function.
-% SCORES = BULKINFCN(X) computes the value of the Bulkin's function at 
-% point X. BULKINFCN accepts a matrix of size M-by-2 and returns a  
+% Computes the value of the Bulkin N. 6 benchmark function.
+% SCORES = BULKINN6FCN(X) computes the value of the Bulkin N. 6 function at 
+% point X. BULKINN6FCN accepts a matrix of size M-by-2 and returns a  
 % vetor SCORES of size M-by-1 in which each row contains the function value 
 % for the corresponding row of X.
 % For more information please visit: 
@@ -9,9 +9,9 @@
 % Author: Mazhar Ansari Ardeh
 % Please forward any comments or bug reports to mazhar.ansari.ardeh at
 % Google's e-mail service or feel free to kindly modify the repository.
-function scores = bulkinfcn(x)
+function scores = bukinn6fcn(x)
     n = size(x, 2);
-    assert(n == 2, 'Bulkin''s functions is only defined on a 2D space.')
+    assert(n == 2, 'The Bulkin N. 6 functions is only defined on a 2D space.')
     
     X = x(:, 1);
     X2 = X .^ 2;
