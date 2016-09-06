10d78b9c9c0f5732eb5f17780588dda4b503f46c
mzhr <mzhr@mazhr>
2016-09-06 20:37:10 +0430

    Added the page for the Schwefel 2.23 function.

---
+++ _includes/schwefel223fcn.m
@@ -0,0 +1,12 @@
+% Computes the value of the Schwefel 2.23 function.
+% SCORES = SCHWEFEL223FCN(X) computes the value of the Schwefel 2.23 
+% function at point X. SCHWEFEL223FCN accepts a matrix of size M-by-N and 
+% returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for the corresponding row of X.
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
+function scores = schwefel223fcn(x)
+    scores = sum(x .^10, 2);
+end
