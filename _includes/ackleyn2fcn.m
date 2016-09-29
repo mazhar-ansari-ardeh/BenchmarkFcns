1e9b88e84a14fade4e882a9a6033b42de1b8b204
mzhr <mzhr@mazhr>
2016-09-29 17:31:38 +0330

    Added the page for the Ackley N. 2 function.

---
+++ _includes/ackleyn2fcn.m
@@ -0,0 +1,18 @@
+% Computes the value of the Ackley N. 2 function.
+% SCORES = BARTELSCONNFCN(X) computes the value of the Ackley N. 2
+% function at point X. BARTELSCONNFCN accepts a matrix of size M-by-2 and 
+% returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for the corresponding row of X.
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
+function scores = ackleyn2fcn(x)
+    
+    n = size(x, 2);
+    assert(n == 2, 'Ackley N. 2 function is only defined on a 2D space.')
+    X = x(:, 1);
+    Y = x(:, 2);
+    
+    scores = -200 * exp(-0.02 * sqrt((X .^ 2) + (Y .^ 2)));
+end
