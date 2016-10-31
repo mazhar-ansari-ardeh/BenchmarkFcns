d2b913a5e146c1f6a555fa8d347761aab604df00
mzhr <mzhr@mazhr>
2016-10-31 21:29:51 +0330

    Added the Bohachevsky N. 2 Function.

---
+++ benchmarks/bohachevskyn2fcn.m
@@ -0,0 +1,17 @@
+% Computes the value of Bohachevsky N. 2 benchmark function.
+% SCORES = BOHACHEVSKYN2FCN(X) computes the value of the Bohachevsky N. 2
+% function at point X. BOHACHEVSKYN2FCN accepts a matrix of size M-by-N and 
+% returns a vetor SCORES of size M-by-1 in which each row contains the 
+% function value for each row of X.
+% 
+% Author: Mazhar Ansari Ardeh
+% Please forward any comments or bug reports to mazhar.ansari.ardeh at
+% Google's e-mail service or feel free to kindly modify the repository.
+function scores = bohachevskyn2fcn(x)
+    n = size(x, 2);
+    assert(n == 2, 'The Bohachevsky N. 2 function is only defined on a 2D space.')
+    X = x(:, 1);
+    Y = x(:, 2);
+    
+    scores = (X .^ 2) + (2 * Y .^ 2) - (0.3 * cos(3 * pi * X)) .* (0.4 * cos(4 * pi * Y)) + 0.3;
+end
